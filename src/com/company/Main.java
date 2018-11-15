d8459d6994d69aa84b9a0071bfa762adff184801
Jacob Cedar <jcedar8640@bths.edu>
2018-11-15 11:39:38 -0500

    asdasd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         int[] nueces = {3 ,6 ,9, 1,2, 7, 8, 11, 23};
+        selectionSort(nueces);
         for(int m = 0; m < nueces.length; m++){
             System.out.println(nueces[m]);
         }
     }
+
     public static void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
     public static void selectionSort(int[] arr){
-        //change
+        //for
+        for(int i = 0; i < arr.length; i++){
+            int min = i;
+            for(int j = 0; j < arr.length-1; j++){
+                if(arr[j] > arr[min]){
+                    min = j;
+                }
+                swap(arr, i, min);
+            }
+        }
     }
 }
